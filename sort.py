b2b95eadcad786475af371434fb806cd88a28e89
MasterYi <62427029+MasterYiCHN@users.noreply.github.com>
2020-04-07 18:34:44 +0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -2,10 +2,10 @@ test_list=[6,2,7,4,12,13,99]
 
 # 冒泡排序
 
-# for i in range(len(test_list)-1):
-#     for j in range(len(test_list)-1):
-#         if test_list[j] > test_list[j+1]:
-#             test_list[j],test_list[j+1]=test_list[j+1],test_list[j]
+for i in range(len(test_list)-1):
+    for j in range(len(test_list)-1):
+        if test_list[j] > test_list[j+1]:
+            test_list[j],test_list[j+1]=test_list[j+1],test_list[j]
 
 # 创建排序函数（返回一个新列表)
 def sort(a):
